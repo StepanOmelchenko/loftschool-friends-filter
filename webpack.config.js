fcb11312db49ab147df9094cd50670a0edb46556
Stepan <omelchenko.stepan@gmail.com>
2018-05-23 21:53:09 +0600

    #1 base template

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
     },
     plugins: [
