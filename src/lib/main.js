84e47bb9f192a02e8032ffaacb591ee4d268b026
Stepan <omelchenko.stepan@gmail.com>
2018-05-26 23:01:48 +0600

    #2 friends to fragment to document func ready

--- src/lib/main.js
+++ src/lib/main.js
@@ -28,12 +28,31 @@ function callAPI(method, params) {
     })
 }
 
+function createFriendsList(list) {
+    let temp = document.querySelector('#friend-template').innerHTML;
+    let leftContainer = document.querySelector('#left-container');
+    let fragment = document.createDocumentFragment();
+    let friend = document.createElement('div');
+
+    friend.innerHTML = temp;
+
+    list.forEach((man) => {
+        let newFriend = friend.cloneNode(true);
+        newFriend.querySelector('.friends__friend-img').style.backgroundImage = `url(${man.photo_100})`;
+        newFriend.querySelector('.friends__friend-name').innerText = `${man.last_name} ${man.first_name}`;
+        fragment.appendChild(newFriend);
+    });
+    
+    leftContainer.innerHTML = '';
+    leftContainer.appendChild(fragment);
+}
+
 (async () => {
     try {
         await auth();
         const friends = await callAPI('friends.get', { fields: 'photo_100' });
-        
-        console.log(friends);
+        console.log(friends.items);
+        createFriendsList(friends.items);
 
     } catch (error) {
         console.log(error);
