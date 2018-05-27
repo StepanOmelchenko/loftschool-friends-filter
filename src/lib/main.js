b5ee9cc428bf13a04e111577a40a29d575d8c890
Stepan <omelchenko.stepan@gmail.com>
2018-05-27 22:01:21 +0600

    #2 VK func's separated

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,40 +1,48 @@
-VK.init({
-    apiId: 6490638
-});
+import {auth, callAPI} from './dataFromVK';
 
-function auth() {
-    return new Promise((resolve, reject) => {
-        VK.Auth.login(data => {
-            if (data.session) {
-                resolve();
-            } else {
-                reject(new Error('Не удалось авторизоваться'));
-            }
-        }, 2);
-    });
+const leftSearchInput = document.querySelector('#left-search');
+const rightSearchInput = document.querySelector('#right-search');
+const leftContainer = document.querySelector('#left-container');
+const rightContainer = document.querySelector('#right-container');
+const tempLeftFriend = document.querySelector('#friend-template').innerHTML;
+const tempRightFriend = document.querySelector('#friend-moved-template').innerHTML;
+const friendsStore = {
+    allFriends: [],
+    leftList: [],
+    rightList: []
+};
+
+(async () => {
+    try {
+        await auth();
+        const friends = await callAPI('friends.get', { fields: 'photo_100' });
+        console.log(friends.items);
+        friendsStore.allFriends = friends.items;
+        friendsStore.leftList = friends.items;
+        createFriendsList(friendsStore.leftList, leftContainer, tempLeftFriend);
+
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+leftSearchInput.addEventListener('input', friendsFilter);
+
+function isMatching(word, chunk) {
+    let regExp = new RegExp(chunk, 'i');
+
+    return regExp.test(word);
 }
 
-function callAPI(method, params) {
-    params.v = '5.76';
-
-    return new Promise((resolve, reject) => {
-        VK.api(method, params, (data) => {
-            if (data.error) {
-                reject(data.error);
-            } else {
-                resolve(data.response);
-            }
-        });
-    })
+function friendsFilter(e, friendsList, chunk) {
+    console.log(e.target.value);
 }
 
-function createFriendsList(list) {
-    let temp = document.querySelector('#friend-template').innerHTML;
-    let leftContainer = document.querySelector('#left-container');
+function createFriendsList(list, container, tempFriend) {
     let fragment = document.createDocumentFragment();
     let friend = document.createElement('div');
 
-    friend.innerHTML = temp;
+    friend.innerHTML = tempFriend;
 
     list.forEach((man) => {
         let newFriend = friend.cloneNode(true);
@@ -42,19 +50,7 @@ function createFriendsList(list) {
         newFriend.querySelector('.friends__friend-name').innerText = `${man.last_name} ${man.first_name}`;
         fragment.appendChild(newFriend);
     });
-    
-    leftContainer.innerHTML = '';
-    leftContainer.appendChild(fragment);
+
+    container.innerHTML = '';
+    container.appendChild(fragment);
 }
-
-(async () => {
-    try {
-        await auth();
-        const friends = await callAPI('friends.get', { fields: 'photo_100' });
-        console.log(friends.items);
-        createFriendsList(friends.items);
-
-    } catch (error) {
-        console.log(error);
-    }
-})();
