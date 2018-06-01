0c9f118fd3eb9bf38f7383698f84d5c4bd2d67e9
Stepan <omelchenko.stepan@gmail.com>
2018-06-01 20:09:43 +0600

    #3 devServer fixed

--- webpack.config.js
+++ webpack.config.js
@@ -71,8 +71,8 @@ module.exports = {
         ]
     },
     devServer: {
+        contentBase: path.join(__dirname, "dist"),
         compress: true,
-        port: 3000,
-        historyApiFallback: true
+        port: 9000
     }  
 };
